912dd93e428816bb72edb064f38f93f01fec23d4
khuong.hm <khuong.hm@tinasoft.com.vn>
2025-09-09 22:40:35 +0700

    config: setup code

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,8 +5,6 @@ import { toast } from 'sonner';
 import React from 'react';
 
 const Blog = () => {
-  const a = 10;
-
   return (
     <Button className="border border-red rounded-b-sm" onClick={() => toast.success('haha')}>
       click
