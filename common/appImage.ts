4350b5e85466dd52cd85d50372f3daae7e33d4db
khuong.hm <khuong.hm@tinasoft.com.vn>
2025-09-30 00:45:01 +0700

    feat: 500 internal server error page

--- common/appImage.ts
+++ common/appImage.ts
@@ -3,6 +3,7 @@ import logo from '@/public/images/logo.png';
 import avatarFallback from '@/public/images/avatar-fallback.png';
 import bgCompanyProfile from '@/public/images/bg-company-profile.png';
 import notFoundImg from '@/public/images/not-found.png';
+import internalServerErrorImg from '@/public/images/internal-server-eror.png';
 
 export const AppImage = {
   backgroundLogo,
@@ -11,5 +12,6 @@ export const AppImage = {
   bgCompanyProfile,
   ErrorImgs: {
     notFoundImg,
+    internalServerErrorImg,
   },
 };
