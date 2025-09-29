23bffb88e4b9d54bd2a5ad5c636fc9369863e33a
khuong.hm <khuong.hm@tinasoft.com.vn>
2025-09-30 00:34:23 +0700

    feat: 404 not found page

--- common/appImage.ts
+++ common/appImage.ts
@@ -2,10 +2,14 @@ import backgroundLogo from '@/public/images/backgroundLogin.png';
 import logo from '@/public/images/logo.png';
 import avatarFallback from '@/public/images/avatar-fallback.png';
 import bgCompanyProfile from '@/public/images/bg-company-profile.png';
+import notFoundImg from '@/public/images/not-found.png';
 
 export const AppImage = {
   backgroundLogo,
   logo,
   avatarFallback,
   bgCompanyProfile,
+  ErrorImgs: {
+    notFoundImg,
+  },
 };
